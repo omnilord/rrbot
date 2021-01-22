f68368e218e0c856e61b5dd6433f7d574c0f79bb
Eric L. Truitte, MBA <omnilord@privateerconsulting.com>
2021-01-21 23:34:20 -0500

    [WIP] save new ad data

--- src/events/ads_handlers.py
+++ src/events/ads_handlers.py
@@ -167,6 +167,8 @@ def setup(bot):
 
         if channel is not None:
             ad = await AdsMessages.from_discord_message(db_session, message)
+            db_session.add(ad)
+            db_session.commit()
             server = ensure_server(db_session, message.guild.id)
             notice = render_new_ad(ad, message, channel, server.timezone)
             await send_notify(notice, channel, db_session, 'New Ad')
