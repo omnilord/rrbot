621efc2e80553ce21e5eac394b646bfc882a2b3c
Eric L. Truitte, MBA <omnilord@privateerconsulting.com>
2021-02-08 07:41:50 -0500

    [WIP] refactor task scheduler for cleaner code read

--- src/task_scheduler.py
+++ src/task_scheduler.py
@@ -1,37 +1,21 @@
 import asyncio, uuid, logging
-from bot_utils import RRBotException
-
-class TaskSchedulerException(RRBotException):
-    pass
 
 tasks = {}
 
 DEFAULT_TASK_DELAY = 10
 
-def deregister(key, method='pop'):
-    """
-    cancel and clear a key, if it exists.
-
-    Parameters:
-    `key` - the identity of a task to remove from the queue.
-    `method` - <internal use>
-    """
-
-    fn = getattr(tasks, method)
-    task = fn(key, None)
-    if task is not None and not task.cancelled():
-        logging.info(f'Cancelling task for key={key}')
-        task.cancel()
-
 
 async def coroutine_runner(task, key, delay, *args):
-    if delay < 1:
-        raise TaskSchedulerException('Cannot run a task scheduled coroutine with a delay less than 1 second.')
+    """
+    internal execution of the coroutine task and cleanup
+    of task runner artifacts.
+    """
+
     await asyncio.sleep(delay)
-    logging.info(f'Running task for key={key}')
+    logging.info(f'Running coroutine task for key={key}')
     await task(*args)
     tasks.pop(key)
-    logging.info(f'Completing task for key={key}')
+    logging.info(f'Completed coroutine task for key={key}')
     return
 
 
@@ -41,8 +25,10 @@ def runner(task, key, *args):
     of task runner artifacts.
     """
 
+    logging.info(f'Running standard task for key={key}')
     task(*args)
-    deregister(key)
+    tasks.pop(key)
+    logging.info(f'Completed standard task for key={key}')
     return
 
 
@@ -58,7 +44,7 @@ def register(callback, *args, key=None, delay=DEFAULT_TASK_DELAY):
     `args` - the parameterized list of arguments to be passed to the callback
             (no keyword args).
     `key`  - the optional keyword for deduplicating.  If `key` is provided,
-            the task runner with cancel an existing previous task assigned
+            the task runner will cancel an existing previous task assigned
             that key and replace it with a new callback.
     `delay` - how long to wait in seconds before executing the callback.
     """
@@ -79,10 +65,25 @@ def register(callback, *args, key=None, delay=DEFAULT_TASK_DELAY):
     return key
 
 
+def deregister(key, method='pop'):
+    """
+    cancel and clear a key, if it exists.
+
+    Parameters:
+    `key` - the identity of a task to remove from the queue.
+    `method` - <internal use>
+    """
+
+    fn = getattr(tasks, method)
+    task = fn(key, None)
+    if task is not None and not task.cancelled():
+        logging.info(f'Cancelling task for key={key}')
+        task.cancel()
+
+
 def shutdown():
     """
     bot is going down, kill all the tasks
     """
 
-    for task in tasks.values():
-        task.cancel()
+    [task.cancel() for task in tasks.values()]
